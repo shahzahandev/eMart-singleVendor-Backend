ea2ff85ffc3f1c5337fa0dab369cc5c771157abd
shahzahandev <shahzahansiraj516@gmail.com>
2026-07-13 04:10:28 +0600

    single and all order controller

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -18,7 +18,7 @@ exports.paymentController = async (req, res) => {
     let tran_id = firstThreeletter + randomNumber.slice(-5) + ecoName + randomNumber2.slice(-3);
 
     try {
-     const cart = await Cart.find({ user: userId }).populate('product user');
+        const cart = await Cart.find({ user: userId }).populate('product user');
 
         let totalPrice = 0;
         let proInfo = [];
@@ -38,25 +38,25 @@ exports.paymentController = async (req, res) => {
             user = item.user
             totalPrice += item.totalPrice
         });
-        
-    const data = {
-        total_amount: totalPrice,
-        currency: 'BDT',
-        tran_id: tran_id, // use unique tran_id for each api call
-        success_url: 'http://localhost:3030/success',
-        fail_url: 'http://localhost:3030/fail',
-        cancel_url: 'http://localhost:3030/cancel',
-        ipn_url: 'http://localhost:3030/ipn',
-        shipping_method: 'Courier',
-        ship_country: 'Bangladesh',
-        ship_name : cus_name,
-        ship_add1: cus_add1,
-        ship_add2: cus_add2,
-        ship_city: cus_city,
-        ship_state: cus_state,
-        ship_postcode: cus_postcode,
-        ...req.body
-    };
+
+        const data = {
+            total_amount: totalPrice,
+            currency: 'BDT',
+            tran_id: tran_id, // use unique tran_id for each api call
+            success_url: 'http://localhost:3030/success',
+            fail_url: 'http://localhost:3030/fail',
+            cancel_url: 'http://localhost:3030/cancel',
+            ipn_url: 'http://localhost:3030/ipn',
+            shipping_method: 'Courier',
+            ship_country: 'Bangladesh',
+            ship_name: cus_name,
+            ship_add1: cus_add1,
+            ship_add2: cus_add2,
+            ship_city: cus_city,
+            ship_state: cus_state,
+            ship_postcode: cus_postcode,
+            ...req.body
+        };
 
         // Order saving process
         let order = new Order({
@@ -70,13 +70,61 @@ exports.paymentController = async (req, res) => {
         const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
         sslcz.init(data).then(apiResponse => {
             // Redirect the user to payment gateway   
-            let GatewayPageURL = apiResponse.GatewayPageURL 
+            let GatewayPageURL = apiResponse.GatewayPageURL
 
-            return res.status(200).json({
+            return res.status(201).json({
                 success: true,
                 message: 'Order created successfully, Make payment in this URL',
                 payment_link: GatewayPageURL
             });
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: 'Server error',
+            error: error.message
+        });
+    }
+}
+
+exports.singleOrder = async (req, res) => {
+    const { userId } = req.params;
+
+    try {
+        const order = await Order.find({ user: userId });
+        if (!order) {
+            return res.status(404).json({
+                success: false,
+                message: 'Order not found'
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: 'Fetching order successfully',
+            order
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: 'Server error',
+            error: error.message
+        });
+    }
+
+}
+
+exports.allOrder = async (req, res) => {
+    try {
+        const order = await Order.find({});
+        return res.status(200).json({
+            success: true,
+            message: 'Fetching all order successfully',
+            order
         });
 
     } catch (error) {
--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { paymentController } = require('../controllers/paymentController');
+const { paymentController, singleOrder, allOrder } = require('../controllers/paymentController');
 const router = express.Router();
 
 
 router.post('/payment', paymentController);  // checked
+router.get('/singleOrder/:userId', singleOrder);  // checked
+router.get('/allOrder', allOrder);  // checked
 
 module.exports = router;
